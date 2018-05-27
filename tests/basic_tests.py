f0d3695372aaac7e92e92453313dea0533905598
kauboy26 <kaushik.gc98@gmail.com>
2018-05-27 12:20:53 -0400

    just some commenting here

---
+++ tests/basic_tests.py
@@ -0,0 +1,5 @@
+"""
+I'll enter some real tests in here later, after the compiler reaches an acceptable stage.
+For now, I'm screwing around, so the other basic_tests file in the parent directory is in
+use.
+"""
